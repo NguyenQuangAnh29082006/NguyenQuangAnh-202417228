d07502fe2c6d5a930abe1ce05766f1e715873cf9
NguyenQuangAnh29082006 <alphascorpiontd@gmail.com>
2026-05-18 00:18:25 +0700

    Add files via upload

---
+++ AimProject/src/hust/soict/dsai/aims/media/Media.java
@@ -0,0 +1,39 @@
+package hust.soict.dsai.aims.media;
+
+public abstract class Media {
+	private int id;
+    public Media(int id, String title, String category, float cost) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+	private String title;
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	private String category;
+    private float cost;
+}
