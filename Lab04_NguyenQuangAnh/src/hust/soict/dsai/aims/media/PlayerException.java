9dbf172880c2e1d1bc1e904ef5194fd7dfdf008d
NguyenQuangAnh29082006 <alphascorpiontd@gmail.com>
2026-06-01 09:09:04 +0700

    Delete Lab04_NguyenQuangAnh directory

--- Lab04_NguyenQuangAnh/src/hust/soict/dsai/aims/media/PlayerException.java
+++
@@ -1,9 +0,0 @@
-package hust.soict.dsai.aims.media;
-
-public class PlayerException extends Exception {
-    
-    // Constructor that accepts a custom error message string
-    public PlayerException(String message) {
-        super(message);
-    }
-}
